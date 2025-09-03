831cbd050d9050f929774399808d51332ab0e96b
laissa.tavares <laissa.tavares@g.globo>
2025-09-02 21:54:04 -0300

    Modificao

--- crud_alimentos/settings.py
+++ crud_alimentos/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-*&hj%=e=&(-f(_cr^h8nca+$1%0rj@!wjn&8x+f=loap+4zn'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
